b595e2d6b6f74ac29db9289626b1319101543a7c
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-08-12 00:35:13 -0700

    chore: add commitlint to lint commits
    - Add and configure commitlint to lint commits
    - Update prek to run commitlint on commit-msg git hook

---
+++ commitlint.config.mjs
@@ -0,0 +1,32 @@
+/** @import { UserConfig } from "@commitlint/types" */
+
+import { RuleConfigSeverity } from "@commitlint/types";
+
+/**
+ * @satisfies {UserConfig}
+ */
+const commitlintConfig = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "type-enum": [
+            RuleConfigSeverity.Error,
+            "always",
+            // https://github.com/conventional-changelog/commitlint/tree/master/@commitlint/config-conventional#type-enum
+            [
+                "build",
+                "chore",
+                "ci",
+                "docs",
+                "feat",
+                "fix",
+                "perf",
+                "refactor",
+                "revert",
+                "style",
+                "test",
+            ],
+        ],
+    },
+};
+
+export default commitlintConfig;
