4822b8edd43f3f3d884f7feabea07d4f26356f12
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-08-12 18:58:44 -0700

    feat: rewrite broken_link_callback to directly deserialize/serialize struct

---
+++ .changeset/honest-cars-chew.md
@@ -0,0 +1,5 @@
+---
+"comrak-wasm": patch
+---
+
+feat: rewrite broken_link_callback to directly deserialize/serialize structs
--- src/options/broken_link_callback.rs
+++ src/options/broken_link_callback.rs
@@ -8,23 +8,22 @@ use std::sync::Arc;
 use tsify::Tsify;
 use wasm_bindgen::prelude::*;
 
-struct BrokenLinkCallback(js_sys::Function);
-
-// TODO: There has be a better way to do this. `BrokenLinkReference` I
-// understand needing a separate struct, since its members are type `&'l str`,
-// but there should be a way to deserialize `ResolvedReference` into its comrak
-// struct without the intermediary.
-
 #[derive(Tsify, Serialize)]
-struct BrokenLinkReference {
+#[serde(remote = "ComrakBrokenLinkReference")]
+struct BrokenLinkReference<'l> {
     /// The normalized reference link label. Unicode case folding is applied;
     /// see <https://github.com/commonmark/commonmark-spec/issues/695> for a
     /// discussion on the details of what this exactly means.
-    pub normalized: String,
+    pub normalized: &'l str,
 
     /// The original text in the link label.
-    pub original: String,
+    pub original: &'l str,
 }
+
+#[derive(Serialize)]
+struct BrokenLinkReferenceHelper<'l>(
+    #[serde(with = "BrokenLinkReference")] ComrakBrokenLinkReference<'l>,
+);
 
 /*
  * Due to how structs are formatted data-wise, both `url` and `title` have to be
@@ -32,6 +31,7 @@ struct BrokenLinkReference {
  * not be present in the output
  */
 #[derive(Tsify, Deserialize)]
+#[serde(remote = "ComrakResolvedReference")]
 struct ResolvedReference {
     /// The destination URL of the reference link.
     pub url: String,
@@ -39,6 +39,11 @@ struct ResolvedReference {
     /// The text of the link.
     pub title: String,
 }
+
+#[derive(Deserialize)]
+struct ResolvedReferenceHelper(#[serde(with = "ResolvedReference")] ComrakResolvedReference);
+
+struct BrokenLinkCallback(js_sys::Function);
 
 impl ComrakBrokenLinkCallback for BrokenLinkCallback {
     fn resolve(
@@ -49,11 +54,8 @@ impl ComrakBrokenLinkCallback for BrokenLinkCallback {
             .0
             .call1(
                 &JsValue::UNDEFINED,
-                &serde_wasm_bindgen::to_value(&BrokenLinkReference {
-                    normalized: broken_link_reference.normalized.to_string(),
-                    original: broken_link_reference.original.to_string(),
-                })
-                .ok()?,
+                &serde_wasm_bindgen::to_value(&BrokenLinkReferenceHelper(broken_link_reference))
+                    .ok()?,
             )
             .ok()?;
 
@@ -61,13 +63,11 @@ impl ComrakBrokenLinkCallback for BrokenLinkCallback {
             return None;
         }
 
-        let resolved_reference: ResolvedReference =
-            serde_wasm_bindgen::from_value(js_value).ok()?;
+        let resolved_reference = serde_wasm_bindgen::from_value(js_value)
+            .map(|ResolvedReferenceHelper(resolved_reference)| resolved_reference)
+            .ok()?;
 
-        Some(ComrakResolvedReference {
-            url: resolved_reference.url,
-            title: resolved_reference.title,
-        })
+        Some(resolved_reference)
     }
 }
 
