509bc4f5373e3ad00eebb3241a133d30d573537a
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-08-16 23:04:00 -0700

    test: add integration tests for markdown_to_html_with_plugins

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,6 +1,6 @@
 use comrak_wasm::*;
 
-use wasm_bindgen::JsValue;
+use wasm_bindgen::{JsValue, prelude::wasm_bindgen};
 use wasm_bindgen_test::*;
 
 #[wasm_bindgen_test]
@@ -60,4 +60,40 @@ fn test_markdown_to_commonmark_xml_with_options() {
         markdown_to_commonmark_xml(include_str!("testdata/test-markdown-options.md"), options);
     let commonmark_xml = include_str!("testdata/test-commonmark-xml-options.xml");
     assert_eq!(result.unwrap(), commonmark_xml);
+}
+
+#[wasm_bindgen(inline_js = r#"
+const escapeHtmlTag = (htmlTag) => htmlTag.replace(/[&<>"']/g, c => ({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c]));
+
+export const create_plugins = () => ({
+    render: {
+        codefenceRenderers: new Map([["mermaid", { write: (lang, meta, code) => `<figure class="${lang}" data-meta="${meta}">${code}</figure>\n` } ]]),
+        codefenceSyntaxHighlighter: {
+            writeHighlighted: (lang, code) => `<!--${lang}-->${code}</span>`,
+            writePreTag: (attributes) =>
+                `<pre data-json="${escapeHtmlTag(JSON.stringify(Object.fromEntries(attributes)))}">`,
+            writeCodeTag: (attributes) =>
+                `<code data-json="${escapeHtmlTag(JSON.stringify(Object.fromEntries(attributes)))}">`,
+        },
+        headingAdapter: {
+            enter: (heading) => `<h${heading.level} data-heading="true">`,
+            exit: (heading) => `</h${heading.level}>`,
+        },
+    },
+});
+"#)]
+extern "C" {
+    fn create_plugins() -> JsValue;
+}
+
+#[wasm_bindgen_test]
+fn test_markdown_to_html_with_plugins() {
+    let options = js_sys::JSON::parse(include_str!("testdata/options.json")).unwrap();
+    let result = markdown_to_html_with_plugins(
+        include_str!("testdata/test-markdown-plugins.md"),
+        options,
+        create_plugins(),
+    );
+    let html = include_str!("testdata/test-html-plugins.html");
+    assert_eq!(result.unwrap(), html);
 }
---
+++ tests/testdata/test-markdown-plugins.md
@@ -0,0 +1,11 @@
+# Hello world
+
+This is **Markdown**
+
+```javascript
+console.log("Hello World!");
+```
+
+``` mermaid theme=dark
+graph TD;
+```
